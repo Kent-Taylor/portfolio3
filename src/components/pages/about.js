2ca4570dc11babfe7a2b80a5e5a57ee93343849c
107124 <kredfox4@gmail.com>
2020-01-03 13:40:28 -0700

    added media queries to about and contact page and blog

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -4,28 +4,30 @@ import profile_pic from "../../../static/assets/images/profile_pic.png";
 
 export default function() {
   return (
-    <div className="content-page-wrapper">
-      <div
-        className="left-column"
-        style={{
-          background: "url(" + profile_pic + ") no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
-      />
-      <div className="right-column">
-        <p>
-          "I’ve always had a passion for technology and creating things. Front
-          end design, backend development- I create clean, powerful functions
-          with simple designs. I am passionate about user experience and
-          proficient at UI. My core competency is utilizing technology for
-          problem solving."
-          <p>-Kent</p>
-        </p>
+    <div className="about-container">
+      <div className="content-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            background: "url(" + profile_pic + ") no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+          }}
+        />
+        <div className="right-column">
+          <p>
+            "I’ve always had a passion for technology and creating things. Front
+            end design, backend development- I create clean, powerful functions
+            with simple designs. I am passionate about user experience and
+            proficient at UI. My core competency is utilizing technology for
+            problem solving."
+            <p>-Kent</p>
+          </p>
 
-        <Link to="/contact" className="go-to-contact">
-          go to Contact Info
-        </Link>
+          <Link to="/contact" className="go-to-contact">
+            go to Contact Info
+          </Link>
+        </div>
       </div>
     </div>
   );
