19a5cf55b8b734cf77982fed309d6999f9b57f09
107124 <kredfox4@gmail.com>
2020-02-04 11:37:34 -0700

    Mobile issue with scrolling. Fixed the issue where the first time you try to scroll, it would not work if you're at the very top or very bottom.

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function() {
   return (
     <div class="pdf-container">
-
       <iframe
         src="https://www.keepandshare.com/doc30/109479/resume-12-6-19-pdf-82k?da=y&ifr=y"
         width="60%"
